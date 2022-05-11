0d4e3228d58a9cb21104e58e36c3471e7c3ca9f1
isigurohatune <39hatune.1925@gmail.com>
2022-05-11 18:28:09 +0900

    うるう年か判定するテストを通しました。

--- src/UnitTest/UnitTest.cpp
+++ src/UnitTest/UnitTest.cpp
@@ -14,5 +14,17 @@ namespace UnitTest
 		{
 			Assert::AreEqual(false, is_leap_year(1));
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(true, is_leap_year(2024));
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::AreEqual(false, is_leap_year(2100));
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::AreEqual(true, is_leap_year(2000));
+		}
 	};
 }
